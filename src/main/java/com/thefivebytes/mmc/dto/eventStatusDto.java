5f192d8b124564d8cc403765d19d81ebe2d2d923
dell- <abdelkabir.jabrane@uir.ac.ma>
2023-11-13 18:55:28 +0100

    la class eventStatus

---
+++ src/main/java/com/thefivebytes/mmc/dto/eventStatusDto.java
@@ -0,0 +1,13 @@
+package com.thefivebytes.mmc.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class eventStatusDto {
+    private Long id;
+    private String Label;
+}
